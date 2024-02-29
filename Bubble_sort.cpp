c643463c0b0e7182901609a42cb2107810bfbcf3
Fluidsuit <mehertank1911@gmail.com>
2024-02-29 22:54:43 +0530

    commit

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -1,5 +1,6 @@
 //Bubble sort
-// Time Complexcity :-  
+// Time Complexcity :- Worst and Average = O(n^2)
+//                     Best = O(n) 
 #include<iostream>
 using namespace std;
  
@@ -7,6 +8,7 @@ void Bubble_sort(int arr[],int n)
 {
     for(int i=n-1;i>=0;i--)
     {
+        int didswap=0; // (1) bcs what if input array is in already sorted order 
         for(int j=0;j<=i-i;j++) // it also can be j<=i as last element should not to compare in bubble sort
         {
             if(arr[j]>arr[j+1])
@@ -14,7 +16,13 @@ void Bubble_sort(int arr[],int n)
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
+                didswap= 1;// (1)
             }
+        }
+    
+        if (didswap==0)// (1)
+        {
+            break;
         }
     }
 }
